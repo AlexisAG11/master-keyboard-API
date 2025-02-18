1dd991a573eede39f6ca4d29099ec1ca22223b7f
Alexis <pseudoalex@outlook.fr>
2025-02-18 17:24:09 +0100

    add api re-recall

--- index.js
+++ index.js
@@ -16,11 +16,14 @@ const openai = new OpenAI({
 
 const contentText = "Give me 10 easy words to write, 10 difficult words to write, 10 medium difficulty words and format them in a JSON with only JSON data no useless text and the name of the array are 'easy', 'medium', 'difficult'";
 
+const contentTextReAsking = "This time can you give me new words : 10 easy words to write, 10 difficult words to write, 10 medium difficulty words and format them in a JSON with only JSON data no useless text and the name of the array are 'easy', 'medium', 'difficult'";
+
 app.use(cors({
     origin: 'http://localhost:4200'
   }));
 
 app.use(express.json());
+
 app.get('/gptResponse', async (req, res) => {
     const completion = await openai.chat.completions.create({
         model: "gpt-4o-mini",
@@ -28,6 +31,30 @@ app.get('/gptResponse', async (req, res) => {
             {
                 role: "user",
                 content: contentText,
+            },
+        ],
+        store: true,
+    });
+
+    let responseData = completion.choices[0].message.content;
+    const indexFirst = responseData.indexOf('{');
+    const indexEnd = responseData.indexOf('}');
+    const formatResponse = responseData.slice(indexFirst, indexEnd+1);
+
+    let responseDataJSON = JSON.parse(formatResponse);
+
+    res.status(200).send({
+        data: responseDataJSON
+    })
+})
+
+app.get('/gptResponse2', async (req, res) => {
+    const completion = await openai.chat.completions.create({
+        model: "gpt-4o-mini",
+        messages: [
+            {
+                role: "user",
+                content: contentTextReAsking,
             },
         ],
         store: true,
