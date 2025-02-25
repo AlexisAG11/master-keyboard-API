567dd952308df81a63c5f404f407da2b3d8b0626
Alexis <pseudoalex@outlook.fr>
2025-02-25 18:09:43 +0100

    maj chat prompt

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ const openai = new OpenAI({
     apiKey: API_KEY,
 });
 
-const contentText = "Give me 10 easy words to write, 10 difficult words to write, 10 medium difficulty words and format them in a JSON with only JSON data no useless text and the name of the array are 'easy', 'medium', 'difficult'";
+const contentText = "Give me exactly 30 random words and format them in a JSON with only JSON data no useless text with the name of the array : 'words'";
+// const contentText = "Give me 10 easy words to write, 10 difficult words to write, 10 medium difficulty words and format them in a JSON with only JSON data no useless text and the name of the array are 'easy', 'medium', 'difficult'";
 
-const contentTextReAsking = "This time can you give me new words : 10 easy words to write, 10 difficult words to write, 10 medium difficulty words and format them in a JSON with only JSON data no useless text and the name of the array are 'easy', 'medium', 'difficult'";
 
 app.use(cors({
     origin: 'http://localhost:4200'
@@ -48,28 +48,28 @@ app.get('/gptResponse', async (req, res) => {
     })
 })
 
-app.get('/gptResponse2', async (req, res) => {
-    const completion = await openai.chat.completions.create({
-        model: "gpt-4o-mini",
-        messages: [
-            {
-                role: "user",
-                content: contentTextReAsking,
-            },
-        ],
-        store: true,
-    });
+// app.get('/gptResponse2', async (req, res) => {
+//     const completion = await openai.chat.completions.create({
+//         model: "gpt-4o-mini",
+//         messages: [
+//             {
+//                 role: "user",
+//                 content: contentTextReAsking,
+//             },
+//         ],
+//         store: true,
+//     });
 
-    let responseData = completion.choices[0].message.content;
-    const indexFirst = responseData.indexOf('{');
-    const indexEnd = responseData.indexOf('}');
-    const formatResponse = responseData.slice(indexFirst, indexEnd+1);
+//     let responseData = completion.choices[0].message.content;
+//     const indexFirst = responseData.indexOf('{');
+//     const indexEnd = responseData.indexOf('}');
+//     const formatResponse = responseData.slice(indexFirst, indexEnd+1);
 
-    let responseDataJSON = JSON.parse(formatResponse);
+//     let responseDataJSON = JSON.parse(formatResponse);
 
-    res.status(200).send({
-        data: responseDataJSON
-    })
-})
+//     res.status(200).send({
+//         data: responseDataJSON
+//     })
+// })
 
 app.listen(PORT,(console.log(`Server is istening on port ${PORT}`)))
